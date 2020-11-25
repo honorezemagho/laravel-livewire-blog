28e67ec9d4f8513c63850a93ff1026a2d7eef17d
honorezemagho <honorezemagho@gmail.com>
2020-11-25 08:21:34 +0100

    add header component to be available globally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel Blog')</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css">
     <link rel="stylesheet" href="{{ asset('./css/tailwind.min.css') }}">
     @livewireStyles
 </head>
 <body>
+
+    <x-header></x-header>
 
     @yield('content')
 
